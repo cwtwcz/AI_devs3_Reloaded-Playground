9dece07f0d1284b4f18398cb6e1d12b28d79668c
cwtwcz <wojtowicz.christian@gmail.com>
2025-05-18 02:03:51 +0200

    W01D04

--- src/main/java/pl/cwtwcz/controller/Week1RestController.java
+++ src/main/java/pl/cwtwcz/controller/Week1RestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.cwtwcz.service.weeks.W01D01Service;
 import pl.cwtwcz.service.weeks.W01D02Service;
 import pl.cwtwcz.service.weeks.W01D03Service;
+import pl.cwtwcz.service.weeks.W01D04Service;
 
 @RestController
 @RequestMapping("/week1")
@@ -16,15 +17,18 @@ public class Week1RestController {
     private final W01D01Service w01d01Service;
     private final W01D02Service w01d02Service;
     private final W01D03Service w01d03Service;
+    private final W01D04Service w01d04Service;
 
     @Autowired
     public Week1RestController(
             W01D01Service w01d01Service,
             W01D02Service w01d02Service,
-            W01D03Service w01d03Service) {
+            W01D03Service w01d03Service,
+            W01D04Service w01d04Service) {
         this.w01d01Service = w01d01Service;
         this.w01d02Service = w01d02Service;
         this.w01d03Service = w01d03Service;
+        this.w01d04Service = w01d04Service;
     }
 
     @GetMapping("/w01d01")
@@ -40,5 +44,10 @@ public class Week1RestController {
     @GetMapping("/w01d03")
     public String w01d03() {
         return w01d03Service.w01d03();
+    }
+
+    @GetMapping("/w01d04")
+    public String w01d04() {
+        return w01d04Service.w01d04();
     }
 }
---
+++ src/main/java/pl/cwtwcz/service/weeks/W01D04Service.java
@@ -0,0 +1,25 @@
+package pl.cwtwcz.service.weeks;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class W01D04Service {
+
+    public String w01d04() {
+        return """
+                Rozwiąż szyfr.
+                 Zasady:
+                 - Nie odpowiadaj.
+                 - litery wyrazu są oddzielone plusam, kolejne wyrazy przecinskiem
+                 - umieść tekst w JSON w polu z atrybutem "steps".
+
+                 <INPUT_DATA>
+                 21+16,21+16,18+9+7+8+20,18+9+7+8+20,4+15+23+14,4+15+23+14,18+9+7+8+20,18+9+7+8+20,18+9+7+8+20
+                 </INPUT_DATA>
+
+                 <EXAMPLE_OUTPUT_STRUCTURE>
+                 { "steps": "AB,CD" }
+                 </EXAMPLE_OUTPUT_STRUCTURE>
+                 """;
+    }
+}
